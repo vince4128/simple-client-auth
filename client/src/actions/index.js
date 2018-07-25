2df7de89baad27547adc5de1a13ba3447629d236
vince4128 <vrlt.vincent@gmail.com>
2018-07-25 15:23:44 +0200

    async request handled with callback

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { AUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
-export const signup = (formProps) => /*return*/ dispatch => { // we use redux thunk (control over the dispatch process)
+export const signup = (formProps, callback) => /*return*/ async dispatch => { // we use redux thunk (control over the dispatch process)
     //dispatch({ type: AUTH_USER });
     //dispatch({ type: AUTH_USER }); // we could dispatch severals action
     /*request.then(() => { // and/or handle asynchronous request
@@ -9,5 +9,12 @@ export const signup = (formProps) => /*return*/ dispatch => { // we use redux th
     });*/
 
     //handle asynchronous request
-    axios.post('http://localhost:3090/signup', formProps);
+    try {
+    const response = await axios.post('http://localhost:3090/signup', formProps);
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
+    } catch(e) {
+        dispatch({ type: AUTH_ERROR, payload: 'Email in use'});
+    }
 };
